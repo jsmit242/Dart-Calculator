c76b91822578094cf0357f97aa8fad9f57c77c0e
Jared Smit <jsmit@tmcdesign.com>
2020-07-13 14:45:46 -0600

    created dice and changed design

--- app.js
+++ app.js
@@ -9,3 +9,27 @@ function clearValue(){
     sum = 0;
     document.querySelector("#sumValue").value = null;
 }
+
+function rollDice(dice){
+    var roll = 0;
+    var id = "";
+    console.log(dice)
+    if (dice == 1){
+        id = "#dice1";
+    }
+    else{
+        id = "#dice2";
+    }
+    console.log(id)
+    roll = Math.floor(Math.random()*20 + 1)
+    document.querySelector(id).value = roll;
+    console.log("Roll " + dice + ": " + roll)
+}
+
+// roll dice on enter
+document.onkeydown = function(e){
+    if(e.keyCode == 13){
+      rollDice(1);
+      rollDice(2);
+    }
+ };
